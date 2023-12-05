12d49b925dca85a6467b43db354d49a7c0abab3d
Bakha <tursunovb18@gmail.com>
2023-12-05 13:18:35 +0500

    add: HomeWorks

--- практика/lesson6(enum)/Program.cs
+++ практика/lesson6(enum)/Program.cs
@@ -1,77 +1,81 @@
-﻿using System;
-using System.Text;
+﻿//using System;
+//using System.Text;
 
-string str = "Hi!";
-str = "Hello" + ", World!";
+//string str = "Hi!";
+//str = "Hello" + ", World!";
 
-StringBuilder stringBuilder = new();
-stringBuilder.Append("Hello");
-stringBuilder.Append(",");
-stringBuilder.Append(" World!"); // , World!
-stringBuilder.Remove(0, 5);
+//StringBuilder stringBuilder = new();
+//stringBuilder.Append("Hello");
+//stringBuilder.Append(",");
+//stringBuilder.Append(" World!"); // , World!
+//stringBuilder.Remove(0, 5);
 
-string s = stringBuilder.ToString();
+//string s = stringBuilder.ToString();
 
-// operators.
+//// operators.
 
-// = - assigment.
-float floatNumber = 3.14;
-float floatNumber2 = floatNumber % 5;
+//// = - assigment.
+//float floatNumber = 3.14;
+//float floatNumber2 = floatNumber % 5;
 
-// unary operators.
-int negativeNumber = -10;
-int positiveNumber = +negativeNumber; // +10
-negativeNumber = -positiveNumber; // -10
+//// unary operators.
+//int negativeNumber = -10;
+//int positiveNumber = +negativeNumber; // +10
+//negativeNumber = -positiveNumber; // -10
 
-int i = 0;
-i++; // 1
-i--; // 0
+//int i = 0;
+//i++; // 1
+//i--; // 0
 
-int pref_i = 3; // 4
-int pref_result = ++pref_i; // 4
+//int pref_i = 3; // 4
+//int pref_result = ++pref_i; // 4
 
-int pref_dec_i = 7; // 6
-int pref_dec_result = --pref_dec_i; // 6
+//int pref_dec_i = 7; // 6
+//int pref_dec_result = --pref_dec_i; // 6
 
 
-bool isTrue = true;
-bool isFalse = !isTrue; // false
-isTrue = !isFalse; // true
+//bool isTrue = true;
+//bool isFalse = !isTrue; // false
+//isTrue = !isFalse; // true
 
-int number2 = 5;
-int result = ~number2; // -6
-// 00000101 == 5
-// 11111010 == -6
+//int number2 = 5;
+//int result = ~number2; // -6
+//// 00000101 == 5
+//// 11111010 == -6
 
-// logical oper.
-bool condition1 = false;
-bool condition2 = true;
+//// logical oper.
+//bool condition1 = false;
+//bool condition2 = true;
 
-// logical AND.
-bool result = condition1 && condition2;
+//// logical AND.
+//bool result = condition1 && condition2;
 
-// logical OR.
-bool orResult = condition1 || condition2;
+//// logical OR.
+//bool orResult = condition1 || condition2;
 
-// logical exceptional OR. ^
-bool exResult = condition1 ^ condition2;
+//// logical exceptional OR. ^
+//bool exResult = condition1 ^ condition2;
 
-// const.
-int number = 10;
-number = 20;
+//// const.
+//int number = 10;
+//number = 20;
 
-const float PI = 3.14;
-// PI = 5; // Error!
+//const float PI = 3.14;
+//// PI = 5; // Error!
 
-// Enumerations Flags
-Permission guestUserPermission = Permission.Read;
-Permission adminUserPermission = Permission.Write | Permission.Read;
+//// Enumerations Flags
+//Permission guestUserPermission = Permission.Read;
+//Permission adminUserPermission = Permission.Write | Permission.Read;
 
-bool hasRWPermission = adminUserPermission.HasFlag(Permission.Write);
+//bool hasRWPermission = adminUserPermission.HasFlag(Permission.Write);
 
-[Flags]
-enum Permission
-{
-    Read,
-    Write
-};
+//[Flags]
+//enum Permission
+//{
+//    Read,
+//    Write
+//};
+
+
+int myInt = myFloat;
+float myFloat = 25.4F;
