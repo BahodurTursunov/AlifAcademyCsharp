fd75c97ab60af9b171da70ceca64bb17e89ec710
Bakha <tursunovb18@gmail.com>
2023-12-08 11:14:55 +0500

    add: hw7-3

---
+++ homeworks/HW/HW7-3/Program.cs
@@ -0,0 +1,8 @@
+﻿//3. Создайте строку и используйте цикл для вывода каждого символа строки на новой строке.
+
+string str = "Hello, world!";
+
+for (int i = 0; i < str.Length; i++)
+{
+    Console.WriteLine(str[i]);
+}
