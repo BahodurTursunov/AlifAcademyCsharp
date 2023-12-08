0b21484afef51940d02a7ee0883a86e4643e157d
Bakha <tursunovb18@gmail.com>
2023-12-08 12:37:07 +0500

    add

---
+++ homeworks/HW/HW7-1/Program.cs
@@ -0,0 +1,21 @@
+﻿//. Напишите программу, которая проверяет, содержится ли заданное число в массиве.
+
+int[] arr = { 2, 3, 4, 5, 6, 5, 4, 3, 2, 3, 45, 56, 6, 45, 4 };
+int isHaveNumInArr = int.Parse(Console.ReadLine());
+int counter = 0;
+
+foreach (var num in arr)
+{
+    if (isHaveNumInArr == num)
+    {
+        counter++;
+    }
+}
+Console.WriteLine($"Заданное число в массиве встречается {counter} раза");
+
+// 2 вариант
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (isHaveNumInArr == arr[i]) counter++;
+//}
+//Console.WriteLine($"Заданное число в массиве встречается {counter} раза");
---
+++ практика/lesson7(Array)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+int[] arr = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine(arr[2]);
+
+MyInlineArray inlineArray = new();
+
+inlineArray[0] = 10;
+inlineArray[1] = 10;
+inlineArray[2] = 10;
+inlineArray[3] = 10;
+inlineArray[4] = 10;
+inlineArray[5] = 10;
+inlineArray[6] = 10;
+
+[System.Runtime.CompilerServices.InlineArray(length:10)]
+public struct MyInlineArray
+{
+    private int element;
+}
