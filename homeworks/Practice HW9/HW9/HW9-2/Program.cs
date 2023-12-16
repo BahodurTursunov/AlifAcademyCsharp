0a7b024f0877529c1b2ae9f6b23ff516874565ce
Bakha <tursunovb18@gmail.com>
2023-12-17 00:34:33 +0500

    add: hw9-1 and hw9-2

---
+++ homeworks/Practice HW9/HW9/HW9-2/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ 2. Реализуйте три метода, каждый из которых выполняет разные операции (например,
+сложение, вычитание, умножение) с использованием делегата. Проверьте эти методы
+вызвав их через делегата и отобразив результаты на экране.
+
+ */
+
+public delegate int MathOperation(int a, int b);
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        MathOperation substraction = new MathOperation(Subtraction);
+        MathOperation multiplication = new MathOperation(Multiplication);
+        MathOperation division = new MathOperation(Division);
+
+        int resSubstraction = PerformOperation(10, 2, Subtraction);
+        Console.WriteLine($"Вычитание: {resSubstraction}");
+
+        int resMultiplication = PerformOperation(10, 2, Multiplication);
+        Console.WriteLine($"Умножение: {resMultiplication}");
+
+        int resDivision = PerformOperation(10, 0, Division);
+        Console.WriteLine($"Деление: {resDivision}");
+
+    }
+
+    static int PerformOperation(int a, int b, MathOperation operation)
+    {
+        return operation(a, b);
+    }
+    static int Subtraction(int a, int b) => a - b;
+
+    static int Multiplication(int a, int b)
+    {
+        if (a == 0 || b == 0)
+        {
+            return 0;
+        }
+        return a * b;
+    }
+
+    static int Division(int a, int b)
+    {
+        if (a == 0 || b == 0)
+        {
+            Console.WriteLine("На ноль делить нельзя");
+        }
+        return a / b;
+    }
+
+
+}
---
+++ homeworks/Practice HW9/HW9/HW9/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * 1. Создайте консольное приложение C#, демонстрирующее использование метода. 
+метод должен принимать два целых числа в качестве входных параметров и возвращать их
+сумму. Вызовите этот метод из метода Main и отобразите результат на
+экран.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int res = Sum(firstNumber, secondNumber);
+        Console.WriteLine(res);
+
+    }
+
+    static int Sum(int firstNumber, int secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+}
