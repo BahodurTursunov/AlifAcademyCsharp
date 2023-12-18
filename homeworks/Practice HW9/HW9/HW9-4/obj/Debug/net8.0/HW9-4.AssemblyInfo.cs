1790eec443e2ff0697baca8b0e51a9c22078da0f
Bakha <tursunovb18@gmail.com>
2023-12-18 23:28:11 +0500

    add: HW9

--- homeworks/Practice HW9/HW9/HW9-4/obj/Debug/net8.0/HW9-4.AssemblyInfo.cs
+++ homeworks/Practice HW9/HW9/HW9-4/obj/Debug/net8.0/HW9-4.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HW9-2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HW9-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HW9-2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HW9-2")]
+[assembly: System.Reflection.AssemblyProductAttribute("HW9-4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HW9-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
