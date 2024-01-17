b2153a8f35c374c7221cf081aaca0f6c59385971
Bakha <tursunovb18@gmail.com>
2024-01-18 00:15:48 +0500

    add: practice hw19

---
+++ homeworks/Practice HW19/Practice HW19/Dog.cs
@@ -0,0 +1,27 @@
+﻿/*
+ 1.  Создайте иерархию классов для представления различных типов животных, таких как "Собака", "Кошка" и "Птица".
+Используйте наследование для создания производных классов от базового класса "Животное".
+Добавьте в базовый класс виртуальный метод "IssueSound", который будет переопределяться в производных классах
+для описания звуков, издаваемых каждым видом животных. Создайте объекты разных классов и вызовите метод "MakeSound"
+для каждого из них через базовый класс.
+ */
+
+namespace Practice_HW19
+{
+    internal class Dog : Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Dog(string name, int age, string color)
+        {
+            Console.WriteLine($"{name}, {age}, {color}");
+        }
+
+        public override void IssueSound()
+        {
+            Console.WriteLine("Gaw Gaw");
+        }
+    }
+}
