4b972cda08c37b6ae892c5cf1117d6c9faab874e
Bakha <tursunovb18@gmail.com>
2024-01-18 00:16:59 +0500

    add: lesson20

---
+++ практика/lesson20/lesson20/INotificationService.cs
@@ -0,0 +1,31 @@
+﻿namespace Notifier;
+
+internal interface INotificationService
+{
+    public void Notify(string sender, string receiver, string message) =>
+        Console.WriteLine($"LOG: From: {sender} -> To: {receiver}, Message: {message}");
+}
+
+public sealed class PushNotificationProvider : INotificationService
+{
+    public void Notify(string sender, string receiver, string message)
+    {
+
+    }
+}
+
+public sealed class SMSNotificationProvider : INotificationService
+{
+    public void Notify(string sender, string receiver, string message)
+    {
+
+    }
+}
+
+public sealed class EmailNotificationProvider : INotificationService
+{
+    public void Notify(string sender, string receiver, string message)
+    {
+
+    }
+}
---
+++ практика/lesson20/lesson20/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lesson20
+{
+    public class BaseEntity
+    {
+        public long id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public sealed class Customer : BaseEntity
+    {
+        public string? Name { get; set; }
+    }
+
+    public sealed class Order : BaseEntity
+    {
+        public string? Description { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
