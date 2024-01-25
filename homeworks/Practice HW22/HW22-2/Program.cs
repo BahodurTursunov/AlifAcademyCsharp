ff038d8c36993d5f5bfc40e1d48cd1eda60c2cb8
Bakha <tursunovb18@gmail.com>
2024-01-25 15:49:04 +0500

    add: практика дз 22

---
+++ homeworks/Practice HW22/HW22-2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HW22_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new()
+            {
+                Title = "Iron man",
+                Author = "Junior"
+            };
+
+            var (bookTitle, bookAuthor) = myBook;
+
+            Console.WriteLine($"Title: {bookTitle} Author: {bookAuthor}");
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public void Deconstruc(out string title, out string author)
+        {
+            title = Title;
+            author = Author;
+        }
+    }
+}
---
+++ homeworks/Practice HW22/Practice HW22/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice_HW22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var (personName, personAge) = GetPersonInfo();
+
+            Console.WriteLine($"Name: {personName}, Age: {personAge}");
+        }
+
+        static (string name, int age) GetPersonInfo() => ("John", 30);
+    }
+}
