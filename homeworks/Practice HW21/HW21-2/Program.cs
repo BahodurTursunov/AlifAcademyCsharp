0d8dd4a79b271f647565c3230ee70c2e2bb29d71
Bakha <tursunovb18@gmail.com>
2024-01-25 15:48:46 +0500

    add: практика 21 дз

---
+++ homeworks/Practice HW21/HW21-2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HW21_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int far = int.Parse(Console.ReadLine());
+            Faringate(far);
+
+        }
+
+        static void Faringate(int x)
+        {
+            _ = x switch
+            {
+                <= 32 => "Вода находится в состоянии льда",
+                >= 32 and < 212 => "Вода в жидком состоянии",
+                > 212 => "Вода находится в парообразном состоянии"
+            };
+        }
+    }
+
+}
+
---
+++ homeworks/Practice HW21/Practice HW21/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice_HW21
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object userInput1 = "Hello";
+            object userInput2 = 2; 
+            PatternMatching(userInput1);
+            PatternMatching(userInput2);
+        }
+
+        static void PatternMatching(object obj)
+        {
+            if (obj is string str)
+                Console.WriteLine($"{str.Length}");
+            else
+                Console.WriteLine("Это не строка");
+        }
+    }
+}
