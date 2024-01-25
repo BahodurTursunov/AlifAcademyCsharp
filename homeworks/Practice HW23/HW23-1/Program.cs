8a7443f72318c347906281fc42fb552287e9f6f4
Bakha <tursunovb18@gmail.com>
2024-01-26 01:12:22 +0500

    add: hw23 try catch finally

---
+++ homeworks/Practice HW23/HW23-1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace HW23_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<< Input first number >>>");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("<<< Input second number >>>");
+            int secondNum = int.Parse(Console.ReadLine());
+            try
+            {
+                int result = firstNum / secondNum;
+                Console.WriteLine($"\n <<< Result: {result} >>>");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error, {ex.Message}");
+            }
+        }
+    }
+}
---
+++ homeworks/Practice HW23/HW23-2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * 2.  Используйте пользовательское исключение, создав свой собственный класс исключений 
+ * (например, MyCustomException), производный от Exception. В своей программе намеренно вызовите это
+ * пользовательское исключение с помощью ключевого слова throw.
+
+ */
+
+namespace HW23_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new MyCustomException("This is my exception");
+            }
+            catch (MyCustomException ex)
+            {
+                Console.WriteLine($"Catch exception: {ex.Message}");
+            }
+        }
+    }
+
+    public class MyCustomException : Exception
+    {
+        public MyCustomException(string message) : base(message)
+        {
+            
+        }
+    }
+}
