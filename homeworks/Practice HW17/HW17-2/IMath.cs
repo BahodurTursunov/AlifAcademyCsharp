c5891629a1feceed3eac4e4321b580a72df2bd3b
Bakha <tursunovb18@gmail.com>
2024-01-25 18:11:08 +0500

    changes:

--- homeworks/Practice HW17/HW17-2/IMath.cs
+++ homeworks/Practice HW17/HW17-2/IMath.cs
@@ -2,8 +2,8 @@
 {
     public interface IMath
     {
-        static abstract int Add(int a, int b);
-        static abstract int Multiply(int a, int b);
-
+        //static abstract int Add(int a, int b);
+        //static abstract int Multiply(int a, int b);
+        static int Add(int x, int y) {  return x + y; }
     }
 }
--- homeworks/Practice HW17/HW17-2/Program.cs
+++ homeworks/Practice HW17/HW17-2/Program.cs
@@ -4,11 +4,30 @@
     {
         static void Main(string[] args)
         {
-            var resultAdd = Calculator.Add(5, 6);
-            Console.WriteLine(resultAdd);
+            //var resultAdd = Calculator.Add(5, 6);
+            //Console.WriteLine(resultAdd);
 
-            var resultMultiplly = Calculator.Multiply(5, 6);
-            Console.WriteLine(resultMultiplly);
+            //var resultMultiplly = Calculator.Multiply(5, 6);
+            //Console.WriteLine(resultMultiplly);
+
+            //Exception
+            J();
+
+            int[] number = { 1, 2, 3, 4, 5, 6 };
+
+            try
+            {
+                Console.WriteLine(number[8]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Не получилось");
+            }
+
+            void J()
+            {
+                J();
+            }
         }
     }
 }
