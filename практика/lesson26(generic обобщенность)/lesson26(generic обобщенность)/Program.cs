a519a00e4dd7ff13e4fb2fad13aa4cdca0947f43
Bakha <tursunovb18@gmail.com>
2024-01-27 17:49:32 +0500

    ADD:project generic

---
+++ практика/lesson26(generic обобщенность)/lesson26(generic обобщенность)/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace lesson26_generic_обобщенность_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new("Apple");
+            string item = box.GetItem();
+            Console.WriteLine(item);
+
+            Box<int> boxNumber = new(123);
+            boxNumber.SetItem(32);
+            int number = boxNumber.GetItem();
+            Console.WriteLine(number);
+        }
+    }
+
+    public sealed class Box<T>
+    {
+        private T _item;
+
+        public Box(T initialItem)
+        {
+            _item = initialItem;
+        }
+
+        public T GetItem()
+        {
+            Console.WriteLine("Получение: " + _item);
+            return _item;
+        }
+
+        public void SetItem(T item)
+        {
+            Console.WriteLine("Установка: " + _item);
+            _item = item;
+        }
+    }
+}
