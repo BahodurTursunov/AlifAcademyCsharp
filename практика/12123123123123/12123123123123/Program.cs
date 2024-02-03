5bd17a08e6b5aaac215bee8b35440c9a96d6c381
Bakha <tursunovb18@gmail.com>
2024-02-03 17:29:17 +0500

    добавлено продолжение урока

---
+++ практика/12123123123123/12123123123123/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _12123123123123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано вещественное число A и целое число N (>0). Используя один цикл, вывести все целые степени числа A от 1 до N.
+
+            float a = 15.5f;
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ практика/lesson 27/lesson27 (коллекции, продолжение)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace lesson27__коллекции__продолжение_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new() { "1salam", "1aleykum!" };
+
+            List<string> list2 = ["2first", "2second", "2third"];
+
+            List<string> list3 = new();
+            //List<string> list4 = new(capacity: 10) ;
+            //Console.WriteLine("Count:" + list3.Count);
+
+
+
+
+            list3.Add("3First");
+
+            list3.AddRange(list); // позволяет добавлять множество значений
+            list3.AddRange(list2); // позволяет добавлять множество значений
+
+            list3[0] = "3Hello";
+            foreach (var item in list3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(list3[2]);
+        }
+    }
+}
