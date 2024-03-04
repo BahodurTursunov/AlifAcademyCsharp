a44ffb6d73b68b4903eb36025b5adbb167e31545
Bakha <tursunovb18@gmail.com>
2024-03-04 20:18:07 +0500

    added hw 28

---
+++ homeworks/Practice HW28/Practice HW28/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice_HW28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+            
+        }
+    }
+}
