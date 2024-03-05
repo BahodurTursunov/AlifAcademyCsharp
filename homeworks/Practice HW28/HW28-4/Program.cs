f34f5abb7990284df8d525231359cf1465bf7260
Bakha <tursunovb18@gmail.com>
2024-03-06 01:16:02 +0500

    add: hw28-4 queue

---
+++ homeworks/Practice HW28/HW28-4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HW28_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Bakha");
+            queue.Enqueue("Islam");
+            queue.Enqueue("Amriddin");
+            queue.Enqueue("Murodjon");
+            queue.Enqueue("Azimjon");
+
+            Console.WriteLine("Исходная очередь");
+            DisplayInfo(queue);
+            Console.WriteLine();
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine("Очередь после обслуживания клиента -> " + queue.Peek());
+                queue.Dequeue();
+                DisplayInfo(queue);
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine("Очередь пуста");
+        }
+
+        static void DisplayInfo(Queue<string> queue)
+        {
+            foreach (var item in queue)
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+    }
+}
