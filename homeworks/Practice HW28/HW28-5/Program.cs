60756ba369f0ce1c2a0df51c9e63c3f6037d32ab
Bakha <tursunovb18@gmail.com>
2024-03-06 01:32:14 +0500

    add: hw28-5 sortedlist

---
+++ homeworks/Practice HW28/HW28-5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HW28_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, string> books = new SortedList<string, string>();
+
+            books.Add("123456789", "Война и мир");
+            books.Add("1234567", "Преступление и наказание");
+            books.Add("12345678", "Мастер и Маргарита");
+
+            Console.WriteLine("Список книг:");
+            DisplayBooks(books);
+
+            books.Remove("1234567");
+
+            Console.WriteLine("\nСписок книг после удаления:");
+            DisplayBooks(books);
+        }
+
+        static void DisplayBooks(SortedList<string, string> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine($"ISBN: {book.Key}, Название: {book.Value}");
+            }
+        }
+    }
+}
+
+
