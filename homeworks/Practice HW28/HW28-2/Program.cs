979970bce2a66d3d942c8a3768bd8c21c423940a
Bakha <tursunovb18@gmail.com>
2024-03-06 00:30:37 +0500

    add hw28-1/2

---
+++ homeworks/Practice HW28/HW28-2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HW28_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>
+            {
+                "Tom",
+                "Bob",
+                "Alex"
+            };
+
+            Console.WriteLine("Исходный список имен:");
+            DisplayName(names);
+
+            names.Remove("Bob");
+
+            Console.WriteLine("Список имен после удаления:");
+            DisplayName(names);
+        }
+
+        static void DisplayName(List<string> namesList)
+        {
+            foreach (var name in namesList)
+            {
+                Console.WriteLine($"- {name}");
+            }
+        }
+    }
+}
--- homeworks/Practice HW28/Practice HW28/Program.cs
+++ homeworks/Practice HW28/Practice HW28/Program.cs
@@ -4,8 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); 
-            
+            var myArray = new MyArray(5);
+            myArray[0] = 1;
+            Console.WriteLine(myArray[0]);
+
+            myArray[1, 2] = 20;
+            Console.WriteLine(myArray[1, 2]);
         }
+    }
+
+    public class MyArray(int size)
+    {
+        private readonly int[] _array = new int[size];
+        private readonly int[,] _twoDimensionalArray = new int[size, size];
+
+        public int this[int index]
+        {
+            get => _array[index];
+            set => _array[index] = value;
+        }
+
+        public int this[int row, int column]
+        {
+            get => _twoDimensionalArray[row, column];
+            set => _twoDimensionalArray[row,column] = value;
+        }
+
     }
 }
