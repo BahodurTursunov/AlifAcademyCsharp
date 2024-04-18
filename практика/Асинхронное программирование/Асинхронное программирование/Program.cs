9ad98d9f50273d5aaeb26e7c4c01a2582058d3be
Bakha <tursunovb18@gmail.com>
2024-04-18 09:32:58 +0500

    говорили про ассинхронное программирование

---
+++ практика/Асинхронное программирование/Асинхронное программирование/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Coffee coffee = PourCoffee();
+Egg egg = FryEgg();
+Toast toast = ToastBread();
+
+1:02:58 
+Coffee PourCoffee()
+{
+    Console.WriteLine("Pour coffee.....");
+    Thread.Sleep(1000);
+    return new();
+}
+Egg FryEgg()
+{
+    Console.WriteLine("Frying eggs.....");
+    Thread.Sleep(1000);
+    return new();
+}
+
+Toast ToastBread()
+{
+    Console.WriteLine("Toast bread.....");
+    Thread.Sleep(1000);
+    return new();
+}
+
+
+class Coffee{ }
+class Egg { }
+class Toast { }
+
+
